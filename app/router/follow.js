47181f471ff3bc21edbcc04c32d6a79385c00a7f
company <aifeng.dong@caradigmcn.com>
2021-01-22 20:42:23 +0800

    关注

--- app/router/follow.js
+++ app/router/follow.js
@@ -4,9 +4,9 @@ module.exports = (app) => {
 
   const { router,  controller, middleware } = app;
   const jwt = middleware.jwt(app);
-  const { queryFollows, followUserId, cancelFollow, queryFans } = controller.follow;
+  const { queryIdols, followUserId, cancelFollow, queryFans } = controller.follow;
 
-  router.get('/api/queryFollows',jwt, queryFollows);
+  router.get('/api/queryFollows',jwt, queryIdols);
   router.post('/api/follow',jwt, followUserId);
   router.post('/api/cancelFollow',jwt, cancelFollow);
   router.get('/api/queryFans',jwt, queryFans);
--- app/service/follow.js
+++ app/service/follow.js
@@ -4,13 +4,13 @@ const Service = require('egg').Service;
 module.exports = class Follow extends Service {
 
   async query(_id, beFollowId) {
-    const sql = `select * follows where user_id=${_id} and fans_id=${beFollowId}`;
+    const sql = `select * follows where user_id=${beFollowId} and fans_id=${_id}`;
     const result = await this.app.mysql.query(sql);
-    return result;
+    return result || [];
   }
 
   async updateDelFlag(_id, beFollowId, flag) {
-    const sql = `update follows set del_flag = ${flag} where user_id=${_id} and fans_id=${beFollowId}`;
+    const sql = `update follows set del_flag = ${flag} where user_id=${beFollowId} and fans_id=${_id}`;
     const res = await this.app.mysql.query(sql);
     return res;
   }
@@ -22,30 +22,24 @@ module.exports = class Follow extends Service {
     return res;
   }
 
-  async queryByUserId(obj) {
-    let where, colunm;
-    if (obj.user_id) {
-      where = `user_id = ${obj.user_id}`;
-      colunm = `fans_id`;
-    } else {
-      where = `fans_id = ${obj.fans_id}`;
-      colunm = `user_id`;
-    }
+  async queryIdolByUserId(fans_id) {
+  
+    const sql = `SELECT f.user_id, f.del_flag, u.introduce, u.avatar, u.user_name 
+                FROM follows f
+                LEFT JOIN users u
+                ON u.user_id = f.user_id
+                WHERE f.fans_id = ${fans_id} AND f.del_flag = 0`;
+    const result = await this.app.mysql.query(sql);
+    return result;
+  }
 
-    const sql = `SELECT
-                u.user_id,
-                u.introduce,
-                u.avatar,
-                u.user_name,
-                f.del_flag 
-              FROM users u
-              left join follows f
-              on u.user_id = f.user_id
-              WHERE u.user_id IN (
-                SELECT ${colunm} FROM follows
-                WHERE ${where}
-                AND del_flag = 0
-              )`;
+  async queryFansByUserId(user_id) {
+  
+    const sql = `SELECT f.fans_id as user_id, f.del_flag, u.introduce, u.avatar, u.user_name
+              FROM follows f
+              left join users u
+              on u.user_id = f.fans_id
+              WHERE f.user_id = ${user_id} and f.del_flag = 0`;
     const result = await this.app.mysql.query(sql);
     return result;
   }
